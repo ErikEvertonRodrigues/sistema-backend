61d156168bcda12e75566c85e124fb904fad50b6
Erik Everton Rodrigues <erik.webprogrammer@gmail.com>
2026-01-26 00:41:43 -0300

    add login logic and authentication dependecies

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
+from rest_framework.authtoken.models import Token
 from .serializer import UserSerializer
 
 # Create your views here.
@@ -20,3 +21,12 @@ def register(request):
         userRetrieved.save()
 
         return Response(user.data, status=HTTP_201_CREATED)
+    
+
+@api_view(['POST'])
+def login(request):
+    user = User.objects.get(username=request.data['username'])
+
+    if user.check_password(request.data['password']):
+        token = Token.objects.create(user=user)
+        return Response({"message": "Login success", "token": token.key}, status=HTTP_200_OK)
